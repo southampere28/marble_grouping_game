80c32eb5acd98aa8ea9f8e4a1478536a717a1bec
Pramudya <pramudyap333@gmail.com>
2025-10-25 22:39:15 +0700

    add boxes and update marble fill and outlines with dynamic colors

--- lib/features/home/widgets/playground_painter.dart
+++ lib/features/home/widgets/playground_painter.dart
@@ -3,21 +3,53 @@ import 'package:marble_grouping_game/constant/app_color.dart';
 
 class PlaygroundPainter extends CustomPainter {
   final List<Offset> marbles;
-  PlaygroundPainter(this.marbles);
+  List<Color?> marbleFill;
+  List<Color?> marbleOutline;
+  final List<RRect> boxes;
+  final List<Color> boxesBaseColor;
+  final List<Color> boxesFillColor;
+  PlaygroundPainter(
+      {super.repaint,
+      required this.marbles,
+      required this.marbleFill,
+      required this.marbleOutline,
+      required this.boxes,
+      required this.boxesBaseColor,
+      required this.boxesFillColor});
 
   @override
   void paint(Canvas canvas, Size size) {
-    final fillPaint = Paint()
-      ..color = AppColor.thirdColor
-      ..style = PaintingStyle.fill;
-    final outlinePaint = Paint()
-      ..color = AppColor.thirdShadow
-      ..strokeWidth = 2
-      ..style = PaintingStyle.stroke;
 
-    for (final pos in marbles) {
-      canvas.drawCircle(pos, 20, fillPaint);
-      canvas.drawCircle(pos, 20, outlinePaint);
+    // draw boxes
+    for (int i = 0; i < boxes.length; i++) {
+      final box = boxes[i];
+
+      final shadowBox = box.shift(const Offset(4, 4));
+
+      final shadowPaint = Paint()
+        ..color = boxesBaseColor[i]
+        ..style = PaintingStyle.fill;
+
+      final fillPaint = Paint()
+        ..color = boxesFillColor[i]
+        ..style = PaintingStyle.fill;
+
+      canvas.drawRRect(shadowBox, shadowPaint);
+      canvas.drawRRect(box, fillPaint);
+    }
+
+    // draw marble
+    for (int i = 0; i < marbles.length; i++) {
+      final fillPaint = Paint()
+        ..color = marbleFill[i]?? AppColor.thirdColor
+        ..style = PaintingStyle.fill;
+      final outlinePaint = Paint()
+        ..color = marbleOutline[i] ?? AppColor.thirdShadow
+        ..strokeWidth = 2
+        ..style = PaintingStyle.stroke;
+
+      canvas.drawCircle(marbles[i], 20, fillPaint);
+      canvas.drawCircle(marbles[i], 20, outlinePaint);
     }
   }
 
