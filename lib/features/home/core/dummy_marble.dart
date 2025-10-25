c5facafd89a7b0739bddbad0996b9b5cd8282d64
Pramudya <pramudyap333@gmail.com>
2025-10-25 22:39:57 +0700

    feat: create dummy marble offset position list

---
+++ lib/features/home/core/dummy_marble.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/widgets.dart';
+
+class DummyMarble {
+  static List<Offset> dummyOffsetMarbles = [
+    const Offset(80, 50),
+    const Offset(100, 60),
+    const Offset(180, 80),
+    const Offset(260, 90),
+    const Offset(80, 150),
+    const Offset(130, 160),
+    const Offset(210, 170),
+    const Offset(290, 180),
+    const Offset(80, 240),
+    const Offset(120, 250),
+    const Offset(200, 260),
+    const Offset(280, 270),
+    const Offset(80, 320),
+    const Offset(140, 330),
+    const Offset(220, 340),
+    const Offset(300, 350),
+    const Offset(80, 390),
+    const Offset(160, 380),
+    const Offset(240, 370),
+    const Offset(280, 360),
+    const Offset(100, 200),
+    const Offset(150, 100),
+    const Offset(200, 300),
+    const Offset(250, 150),
+  ];
+}
