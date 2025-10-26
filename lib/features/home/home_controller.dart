9365367af6e27fcca1b61b00597e135f8387e9a8
Pramudya <pramudyap333@gmail.com>
2025-10-26 19:18:16 +0700

    feat: separate game logic into controller and add new dialog feature for result check

--- lib/features/home/home_controller.dart
+++ lib/features/home/home_controller.dart
@@ -1,13 +1,18 @@
 import 'dart:math';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:marble_grouping_game/constant/app_color.dart';
 import 'package:marble_grouping_game/features/home/core/dummy_marble.dart';
+import 'package:marble_grouping_game/utils/app_util.dart';
 
 class HomeController extends GetxController {
   // question formula
   var dividen = 24; // I use integer instead of observable cause static value
   var divisor = 3; // -------------------------------------------------
+
+  // answer
+  var correctAnswer = 0.obs;
 
   // index marble while drag
   final RxnInt draggingIndex = RxnInt(null);
@@ -40,17 +45,17 @@ class HomeController extends GetxController {
   // rect boxes with position included
   final boxes = [
     RRect.fromRectAndCorners(
-      Rect.fromLTWH(0, 60, 50, 80),
+      Rect.fromLTWH(0, 50, 50, 100),
       topRight: Radius.circular(8),
       bottomRight: Radius.circular(8),
     ),
     RRect.fromRectAndCorners(
-      Rect.fromLTWH(0, 170, 50, 80),
+      Rect.fromLTWH(0, 170, 50, 100),
       topRight: Radius.circular(8),
       bottomRight: Radius.circular(8),
     ),
     RRect.fromRectAndCorners(
-      Rect.fromLTWH(0, 280, 50, 80),
+      Rect.fromLTWH(0, 290, 50, 100),
       topRight: Radius.circular(8),
       bottomRight: Radius.circular(8),
     ),
@@ -70,11 +75,196 @@ class HomeController extends GetxController {
 
   // === function logic game ===
 
+  // flag audio for play sfx once
+  RxBool audioFlag = false.obs;
+  final stickyPlayer = AudioPlayer();
+  final stickyBoxPlayer = AudioPlayer();
+  final successBoxPlayer = AudioPlayer();
+  final failBoxPlayer = AudioPlayer();
+
+  Future<void> playStickySfx() async {
+    await stickyPlayer.stop();
+    await stickyPlayer.play(AssetSource('audio/stickysfx.mp3'));
+  }
+
+  Future<void> playStickyBoxSfx() async {
+    await stickyBoxPlayer.stop();
+    await stickyBoxPlayer.play(AssetSource('audio/stickyboxsfx.mp3'));
+  }
+
+  Future<void> playSuccessSfx() async {
+    await successBoxPlayer.stop();
+    await successBoxPlayer.play(AssetSource('audio/success.mp3'));
+  }
+
+  Future<void> playFailedSfx() async {
+    await failBoxPlayer.stop();
+    await failBoxPlayer.play(AssetSource('audio/failedsfx.mp3'));
+  }
+
+  // on touch start (choose index marble)
+  void onTouchStart(DragStartDetails details) {
+    final touch = details.localPosition;
+    for (int i = 0; i < marbles.value.length; i++) {
+      if ((touch - marbles.value[i]).distance < 20) {
+        // if dragged marble in group, it will break
+        if (marbleBoxIndex.value[i] != null) {
+          break;
+        } else {
+          draggingIndex.value = i;
+          break;
+        }
+      }
+    }
+  }
+
+  // on pan update (drag marble logic)
+  void onDragUpdate(DragUpdateDetails details) {
+    final idx = draggingIndex.value;
+    if (idx == null) return;
+
+    final groupId = groupIds.value[idx];
+    final delta = details.delta;
+
+    for (int i = 0; i < marbles.value.length; i++) {
+      if (groupIds.value[i] == groupId) {
+        marbles.value[i] += delta;
+      }
+    }
+    marbles.refresh();
+  }
+
+  // sticky marble logic
+  void marbleStick(int draggedIndex) {
+    audioFlag.value = true;
+    const stickyThreshold = 50.0;
+    final draggedPos = marbles.value[draggedIndex];
+    final draggedGroup = groupIds.value[draggedIndex];
+
+    for (int i = 0; i < marbles.value.length; i++) {
+      if (i == draggedIndex) continue;
+
+      final otherPos = marbles.value[i];
+      final distance = (draggedPos - otherPos).distance;
+
+      if (distance < stickyThreshold) {
+        // check if marble connect to marble that have box
+        bool isOtherposInGroup = marbleBoxIndex.value[i] != null;
+
+        // is dragged index in group
+        bool isDraggedIndexInGroup =
+            groupIds.value.where((g) => g == draggedGroup).length > 1;
+
+        if (isOtherposInGroup) {
+          // play audio
+          if (audioFlag.value) {
+            playStickyBoxSfx();
+            audioFlag.value = false;
+          }
+
+          int boxesIdxOtherpos = marbleBoxIndex.value[i]!;
+
+          var box = boxes[boxesIdxOtherpos];
+
+          // joinn in other group
+          final newGroupId = groupIds.value[i];
+
+          // change all marble group into the other's marble group
+          for (int j = 0; j < groupIds.value.length; j++) {
+            if (groupIds.value[j] == draggedGroup) {
+              groupIds.value[j] = newGroupId;
+            }
+          }
+
+          final dir = (draggedPos - otherPos).direction;
+
+          marbles.value[draggingIndex.value!] =
+              otherPos + Offset(35 * cos(dir), 35 * sin(dir));
+
+          final marbleGroupIndices = groupIds.value
+              .asMap()
+              .entries
+              .where((e) => e.value == newGroupId)
+              .map((e) => e.key)
+              .toList();
+
+          // make the marbleboxindex join
+          for (var idx in marbleGroupIndices) {
+            marbleBoxIndex.value[idx] = boxesIdxOtherpos;
+
+            // update fill color and outline color of marble too based on boxes theme
+            marbleColorFill.value[idx] = boxesFillColor[boxesIdxOtherpos];
+            marbleColorOutline.value[idx] = boxesBaseColor[boxesIdxOtherpos];
+          }
+
+          // refresh
+          marbleBoxIndex.refresh();
+          marbleColorFill.refresh();
+          marbleColorOutline.refresh();
+
+          // arrange circles in 2 row horizontal on the right side box
+          const spacing = 35.0;
+          const offsetFromBox = 20.0;
+          for (int j = 0; j < marbleGroupIndices.length; j++) {
+            final rowIndex = j % 2;
+            final colIndex = j ~/ 2;
+
+            final newPos = Offset(
+              box.right + offsetFromBox + colIndex * spacing,
+              box.top + box.height / 2 - spacing / 2 + rowIndex * spacing,
+            );
+
+            marbles.value[marbleGroupIndices[j]] = newPos;
+          }
+        } else {
+          if (isDraggedIndexInGroup) {
+            audioFlag.value = false;
+          }
+
+          if (audioFlag.value) {
+            playStickySfx();
+            audioFlag.value = false;
+          }
+
+          // create new group
+          final newGroupId = groupIds.value[i];
+          for (int j = 0; j < groupIds.value.length; j++) {
+            if (groupIds.value[j] == newGroupId) {
+              groupIds.value[j] = draggedGroup;
+            }
+          }
+
+          final dir = (otherPos - draggedPos).direction;
+          marbles.value[i] = draggedPos + Offset(35 * cos(dir), 35 * sin(dir));
+        }
+      }
+    }
+
+    resolveOverlap();
+    marbles.refresh();
+    groupIds.refresh();
+    draggingIndex.value = null;
+  }
+
+  void checkAnswer(BuildContext context) {
+    var result = resultCheck();
+    AppUtil.dialogResult(context, result,
+        countBox: countBox.value,
+        correctAnswer: correctAnswer.value,
+        reset: reset);
+  }
+
   // checkbox if any marble trying connect box
   void boxCheck(int draggedIndex) {
     for (int i = 0; i < boxes.length; i++) {
       final box = boxes[i];
       if (box.contains(marbles.value[draggedIndex])) {
+        audioFlag.value = true;
+        if (audioFlag.value) {
+          playStickyBoxSfx();
+          audioFlag.value = false;
+        }
+
         // grab all marble that in same group
         final draggedGroup = groupIds.value[draggedIndex];
         final marbleGroupIndices = groupIds.value
@@ -133,11 +323,17 @@ class HomeController extends GetxController {
     }
   }
 
-  // ===========================
+  // === function logic game END ===
 
+  // === additional logic ===
+
+  // answer check
   bool resultCheck() {
     // divider result
     int result = (dividen / divisor).toInt();
+
+    // assign to global variable
+    correctAnswer.value = result;
 
     for (int i = 0; i < boxes.length; i++) {
       final countInBox = marbleBoxIndex.value.where((idx) => idx == i).length;
@@ -148,12 +344,19 @@ class HomeController extends GetxController {
 
     bool allCorrect = countBox.value.every((count) => count == result);
 
+    if (allCorrect) {
+      playSuccessSfx();
+    } else {
+      playFailedSfx();
+    }
+
     // testing data
     print('marble in boxes count -> ${countBox.value}');
 
     return allCorrect;
   }
 
+  // reset state
   void reset() {
     // index marble while drag
     draggingIndex.value = null;
@@ -176,6 +379,7 @@ class HomeController extends GetxController {
 
     // marblebox index to define number of box every marble
     marbleBoxIndex.value = List.filled(24, null);
+    correctAnswer.value = 0;
 
     marbles.refresh();
     marbleColorFill.refresh();
@@ -184,6 +388,8 @@ class HomeController extends GetxController {
     countBox.refresh();
     marbleBoxIndex.refresh();
   }
+
+  // === additional logic END ===
 
   @override
   void onInit() {
--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -7,6 +7,7 @@ import 'package:marble_grouping_game/constant/app_font_style.dart';
 import 'package:marble_grouping_game/features/home/home_controller.dart';
 import 'package:marble_grouping_game/features/home/widgets/math_area_painter.dart';
 import 'package:marble_grouping_game/features/home/widgets/playground_painter.dart';
+import 'package:marble_grouping_game/utils/app_util.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -46,144 +47,22 @@ class HomePage extends StatelessWidget {
                 child: Obx(() {
                   final marbles = controller.marbles;
                   final draggingIndex = controller.draggingIndex;
-                  final groupIds = controller.groupIds;
 
                   return GestureDetector(
                     onPanStart: (details) {
-                      final touch = details.localPosition;
-                      for (int i = 0; i < marbles.value.length; i++) {
-                        if ((touch - marbles.value[i]).distance < 20) {
-                          // if dragged marble in group, it will break
-                          if (controller.marbleBoxIndex.value[i] != null) {
-                            break;
-                          } else {
-                            controller.draggingIndex.value = i;
-                            break;
-                          }
-                        }
-                      }
+                      controller.onTouchStart(details);
                     },
                     onPanUpdate: (details) {
-                      final idx = draggingIndex.value;
-                      if (idx == null) return;
-
-                      final groupId = groupIds.value[idx];
-                      final delta = details.delta;
-
-                      for (int i = 0; i < marbles.value.length; i++) {
-                        if (groupIds.value[i] == groupId) {
-                          marbles.value[i] += delta;
-                        }
-                      }
-                      marbles.refresh();
+                      controller.onDragUpdate(details);
                     },
                     onPanEnd: (_) {
                       final draggedIndex = draggingIndex.value;
                       if (draggedIndex == null) return;
 
                       // Sticky Marble Logic
-                      const stickyThreshold = 50.0;
-                      final draggedPos = marbles.value[draggedIndex];
-                      final draggedGroup = groupIds.value[draggedIndex];
+                      controller.marbleStick(draggedIndex);
 
-                      for (int i = 0; i < marbles.value.length; i++) {
-                        if (i == draggedIndex) continue;
-
-                        final otherPos = marbles.value[i];
-                        final distance = (draggedPos - otherPos).distance;
-
-                        if (distance < stickyThreshold) {
-                          // check if marble connect to marble that have box
-                          bool isOtherposInGroup =
-                              controller.marbleBoxIndex.value[i] != null;
-
-                          if (isOtherposInGroup) {
-                            int boxesIdxOtherpos =
-                                controller.marbleBoxIndex.value[i]!;
-
-                            var box = controller.boxes[boxesIdxOtherpos];
-
-                            // joinn in other group
-                            final newGroupId =
-                                groupIds.value[i]; // grup si other
-
-                            // ubah semua marble yang 1 grup sama dragged menjadi grupnya other
-                            for (int j = 0; j < groupIds.value.length; j++) {
-                              if (groupIds.value[j] == draggedGroup) {
-                                groupIds.value[j] = newGroupId;
-                              }
-                            }
-
-                            final dir = (draggedPos - otherPos).direction;
-
-                            marbles.value[draggingIndex.value!] =
-                                otherPos + Offset(35 * cos(dir), 35 * sin(dir));
-
-                            final marbleGroupIndices = controller.groupIds.value
-                                .asMap()
-                                .entries
-                                .where((e) => e.value == newGroupId)
-                                .map((e) => e.key)
-                                .toList();
-
-                            // make the marbleboxindex join
-                            for (var idx in marbleGroupIndices) {
-                              controller.marbleBoxIndex.value[idx] =
-                                  boxesIdxOtherpos;
-
-                              // update fill color and outline color of marble too based on boxes theme
-                              controller.marbleColorFill.value[idx] =
-                                  controller.boxesFillColor[boxesIdxOtherpos];
-                              controller.marbleColorOutline.value[idx] =
-                                  controller.boxesBaseColor[boxesIdxOtherpos];
-                            }
-
-                            // refresh
-                            controller.marbleBoxIndex.refresh();
-                            controller.marbleColorFill.refresh();
-                            controller.marbleColorOutline.refresh();
-
-                            // arrange circles in 2 row horizontal on the right side box
-                            const spacing = 35.0;
-                            const offsetFromBox = 20.0;
-                            for (int j = 0;
-                                j < marbleGroupIndices.length;
-                                j++) {
-                              final rowIndex = j % 2;
-                              final colIndex = j ~/ 2;
-
-                              final newPos = Offset(
-                                box.right + offsetFromBox + colIndex * spacing,
-                                box.top +
-                                    box.height / 2 -
-                                    spacing / 2 +
-                                    rowIndex * spacing,
-                              );
-
-                              marbles.value[marbleGroupIndices[j]] = newPos;
-                            }
-                          } else {
-                            // create new group
-                            final newGroupId = groupIds.value[i];
-                            for (int j = 0; j < groupIds.value.length; j++) {
-                              if (groupIds.value[j] == newGroupId) {
-                                groupIds.value[j] = draggedGroup;
-                              }
-                            }
-
-                            final dir = (otherPos - draggedPos).direction;
-                            marbles.value[i] = draggedPos +
-                                Offset(35 * cos(dir), 35 * sin(dir));
-                          }
-                        }
-                      }
-
-                      controller.resolveOverlap();
-                      marbles.refresh();
-                      groupIds.refresh();
-                      controller.draggingIndex.value = null;
-
-                      // Boxes Check Logic
+                      // Boxes Check Sticky Logic
                       controller.boxCheck(draggedIndex);
                     },
                     child: SizedBox.expand(
@@ -210,13 +89,7 @@ class HomePage extends StatelessWidget {
                       child: Text('Reset')),
                   TextButton(
                       onPressed: () {
-                        bool resultCheck = controller.resultCheck();
-
-                        if (resultCheck) {
-                          Fluttertoast.showToast(msg: 'Your answer is correct');
-                        } else {
-                          Fluttertoast.showToast(msg: 'Wrong, try again!');
-                        }
+                        controller.checkAnswer(context);
                       },
                       child: Text('Count Boxes')),
                 ],
